9122cd9021ae6c425ef7af934e0b069b9fb9f738
busrageyikahmed <154632304+busrageyikahmed@users.noreply.github.com>
2024-01-19 18:27:01 +0000

    Add files via upload
    added function menuToggle() {
        var x = document.getElementById('myNavtoggle');
        if (x.className === 'navtoggle') {
            x.className += ' responsive';
        } else {
            x.className = 'navtoggle';
          }
        }

--- scripts.js
+++ scripts.js
@@ -1,30 +1,22 @@
 <script src="js/scripts.js"></script>
 console.log("Hi, Welcome to my portfolio! Feel free to browse around and explore. I hope you find something you like.");
 
-// hamburger menu toggle
-function toggleMenu() {
-    const navElement = document.getElementById("myNav");
-
-    if (navElement.classList.contains("closed")) {
-        navElement.classList.remove("closed");
-        navElement.classList.add("open");
+var name = "Busra";
+var age = 24;
+var hobbies = "reading";
+var infoAbout = {
+    name: name,
+    age: age,
+    hobbies: hobbies,
+}
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
     } else {
-        navElement.classList.remove("open");
-        navElement.classList.add("closed");
+        x.className = 'navtoggle';
+      }
     }
-}
 
-// smooth scrolling function
-function smoothScrollToTarget(target) {
-    const headerOffset = 80;
-    const $target = $(target);
-    
-    $('html, body').animate({
-        scrollTop: $target.offset().top - headerOffset
-    }, 1000);
-
-    $target.attr('tabindex', '-1');
-    $target.focus();
-    return false;
-}
-<
+<script   src="https://code.jquery.com/jquery-3.7.1.min.js"  
+     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="   crossorigin="anonymous"></script>
