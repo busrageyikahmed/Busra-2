ba9142f0e34f1afdf31b1c4ac62751d50978f3c2
busrageyikahmed <154632304+busrageyikahmed@users.noreply.github.com>
2024-03-30 15:45:23 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,5 @@
-<script src="js/scripts.js"></script>
-console.log("Hi, Welcome to my portfolio! Feel free to browse around and explore. I hope you find something you like.");
-
-var name = "Busra";
-var age = 24;
-var hobbies = "reading";
-var infoAbout = {
-    name: name,
-    age: age,
-    hobbies: hobbies,
-}
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
+    var x = document.getElementById('myNavToggle');
     if (x.className === 'navtoggle') {
         x.className += ' responsive';
     } else {
@@ -18,5 +7,6 @@ function menuToggle() {
       }
     }
 
-<script   src="https://code.jquery.com/jquery-3.7.1.min.js"  
-     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="   crossorigin="anonymous"></script>
+   
+
+     
